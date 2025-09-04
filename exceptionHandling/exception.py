65aca0bf0df6051439ad99273ba16edec62e367e
n00r-m0hd <noormohd2526@gmail.com>
2025-09-04 21:16:36 +0530

    New Programs

---
+++ exceptionHandling/exception.py
@@ -0,0 +1,10 @@
+# try:
+#     print("Hello I am try block")
+
+# except:
+#     print("Hello, i am a except block")
+
+try:
+    print(x)
+except:
+    print("Exception is occured")
---
+++ fstring.py
@@ -0,0 +1,3 @@
+n=int(input("Enter an Integer\n"))
+
+print(f"{'Even' if n%2==0 else 'Odd'}")
